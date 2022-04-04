c8c9414680797c5b829803e8aa9f1d458f5220df
m-nikita <nikita.m.contact@gmail.com>
2022-04-04 22:27:08 +0200

    add informations section

---
+++ Pong/InformationsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InformationsViewController.swift
+//  Pong
+//
+//  Created by hp omen on 04/04/2022.
+//  Copyright © 2022 hp omen. All rights reserved.
+//
+
+import UIKit
+
+class InformationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
